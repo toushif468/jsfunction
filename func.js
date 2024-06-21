881cc9ec9332834fabb2b9b7bf89b95938692546
toushif468 <toushiforbeebro@gmail.com>
2024-06-21 23:40:28 +0600

    version_one

---
+++ func.js
@@ -0,0 +1,49 @@
+// // understanding fuctions
+
+//     function greet(name) {
+//         console.log('Hello'+name);
+//  }
+
+//  greet('Toushif');
+//  greet('Mary');
+
+
+//  const person1 = {
+//     firstName :'Toushif',
+//     lastName : 'Hasan',
+//     age:28,
+//     isEmployed:true,
+//     sayHello: function() {console.log("hi")},
+//  }
+
+//  const person2 = {
+//     firstName :'Mary',
+//     lastName : 'Smith',
+//     age:20,
+//     isEmployed:false,
+//     sayHello: function() {console.log("hello")},
+
+//  }
+
+// //  console.log(person1.firstName);
+// //  console.log(person2.lastName);
+// //  console.log(person1.isEmployed);
+// //  console.log(person1.age);
+
+// person1.sayHello();
+// person2.sayHello();
+
+
+
+// function greet (){
+
+// };
+
+// const toushif ={
+
+// }
+
+// const niloy =( name) => {
+//     console.log(name);
+// }
+
